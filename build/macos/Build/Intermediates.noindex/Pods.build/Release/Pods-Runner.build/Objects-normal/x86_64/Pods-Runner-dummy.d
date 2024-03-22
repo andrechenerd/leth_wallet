545dbe822548634f5cb24101c577236106677fa9
der <dwa@daw>
2024-03-22 20:25:53 +0200

    -игшдв

--- build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/strozer/Downloads/leathel/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/module.modulemap \
-  /Users/strozer/Downloads/leathel/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/strozer/Downloads/leathel/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/module.modulemap \
-  /Users/strozer/Downloads/leathel/build/macos/Build/Intermediates.noindex/Pods.build/Release/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
